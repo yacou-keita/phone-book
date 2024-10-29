5f803d492d73a5547dec8189f4aa87e94bf70982
Yacou Keita <yacoukeita0212@gmail.com>
2024-10-29 14:02:39 +0000

    add project tamplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { Contact } from './entities/contact';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     ButtonModule, 
+    TableModule,
     RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'phone-book';
+  
+  protected contactList: Contact[] = [
+    { id: 1, firstName: 'John', lastName: 'Doe', phone: '1234567890', email: 'john.doe@example.com', profilePhoto: 'images/profile.jpg' },
+    { id: 2, firstName: 'Jane', lastName: 'Smith', phone: '0987654321', email: 'jane.smith@example.com', profilePhoto: 'images/profile.jpg' },
+    { id: 2, firstName: 'Jane', lastName: 'Smith', phone: '0987654321', email: 'jane.smith@example.com', profilePhoto: 'images/profile.jpg' },
+    { id: 2, firstName: 'Jane', lastName: 'Smith', phone: '0987654321', email: 'jane.smith@example.com', profilePhoto: 'images/profile.jpg' },
+  ];
 }
